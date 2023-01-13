f7d300f5e1ba1e29f1cfbad4b3a23c94677029a2
WhatWR <kul.b@ku.th>
2023-01-13 13:34:51 +0700

    change key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,17 +2,15 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': '8b3daa5e35mshdc6d676f2bdcc1fp164cbcjsnba905643120c',
+    'X-RapidAPI-Key': 'bee37cf4c0msh8e270309560d594p1caa3fjsnb504f861b7f5',
   },
 }
-
-
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': '8b3daa5e35mshdc6d676f2bdcc1fp164cbcjsnba905643120c',
+    'X-RapidAPI-Key': 'bee37cf4c0msh8e270309560d594p1caa3fjsnb504f861b7f5',
   },
 }
 
